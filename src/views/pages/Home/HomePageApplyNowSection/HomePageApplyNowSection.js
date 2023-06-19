5ef79d01f3c439238e6c0c387d8c7180788a47c3
tuhinshuvra <tuhinshuvra18@gmail.com>
2023-06-19 17:25:33 +0600

    added homepage testimonial and applynow section

--- src/views/pages/Home/HomePageApplyNowSection/HomePageApplyNowSection.js
+++ src/views/pages/Home/HomePageApplyNowSection/HomePageApplyNowSection.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import './HomePageApplyNowSection.css';
 
 const HomePageApplyNowSection = () => {
     return (
-        <div>
-            <h2>HomePageApplyNowSection</h2>
+        <div className=' applyNowArea'>
+            <div className=' d-md-flex justify-content-center align-items-center '>
+                <h3 className='testiCardTitle text-center'>Are you ready to realize your dream? </h3>
+                <button className='applyBtn ms-5'>Apply Now</button>
+            </div>
         </div>
     );
 };
--- src/views/pages/Home/HomePageTestimonialSection/HomePageTestimonialSection.js
+++ src/views/pages/Home/HomePageTestimonialSection/HomePageTestimonialSection.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import TestimonialImage from '../../../../assets/testimonialImage.png';
 import './HomePageTestimonialSection.css';
 
 const HomePageTestimonialSection = () => {
     return (
-        <div>
-            <h2>HomePageTestimonialSection</h2>
+        <div className='testimonialArea d-md-flex'>
+            <div className='col-md-5 tesimonialImage'>
+                <img className='testimonialImage' src={TestimonialImage} alt="" />
+            </div>
+            <div className='col-md-7 tesimonialTextArea'>
+                <div class="testimonialCardOne p-5 mx-auto">
+                    <h3 className='testiCardTitle text-center'>Testimonials</h3>
+                    <p className='testiCardSubTitle text-center'>Please check the testimonials from our satisfied clients to confirm.</p>
+                </div>
+
+                <div class="testimonialCardTwo p-5 mx-auto">
+                    <p className='testiCardSubTitle'>I am absolutely thrilled with the service i’ve received from the Excellence team.
+                        they are truly per excellence, you should without a doubt apply to their program and achieve your dream.  </p>
+                </div>
+
+            </div>
         </div>
     );
 };
