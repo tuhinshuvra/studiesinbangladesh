f153196c143b509b170260d7fcfba5f708a910f3
tuhinshuvra <tuhinshuvra18@gmail.com>
2023-06-19 11:35:28 +0600

    implemented featured univerities section

--- src/views/components/GridCarousol/GridCarousol.js
+++ src/views/components/GridCarousol/GridCarousol.js
@@ -1,161 +1,72 @@
 import React from 'react';
 import GridCarousolItem from './GridCarousolItem';
 
-
+import CollegeOne from '../../../assets/mediclaCollegeLogo/bangladeshMedicalCollege.png'
+import CollegeTwo from '../../../assets/mediclaCollegeLogo/enamMedicalCollege.png'
+import CollegeThree from '../../../assets/mediclaCollegeLogo/greenLifeCollege.png'
+import CollegeFour from '../../../assets/mediclaCollegeLogo/magOsmaniMedicalCollege.jpg'
+import CollegeFive from '../../../assets/mediclaCollegeLogo/rajshahiMedicalCollege.png'
+import CollegeSix from '../../../assets/mediclaCollegeLogo/popularMedicalCollege.png'
+import CollegeSeven from '../../../assets/mediclaCollegeLogo/shahidSuhrawardyMedicalCollege.png'
+import CollegeEight from '../../../assets/mediclaCollegeLogo/redcrecentMedicalCollege.jpg'
+import CollegeNine from '../../../assets/mediclaCollegeLogo/usBanglaMedicalCollege.png'
 
 import './GridCarousol.css'
 
 const GridCarousol = () => {
 	// fareOne, fareTwo, fareThree, fareFive
 	return (
-		<div className=' col-md-11 mx-auto'>
+		<div className=' col-md-12 mx-auto '>
 			{/* ########################## domestic locations flights ################################ */}
 
-			<h1 className=' text-center fw-bold pt-lg-5  location_heading '> Popular Locations </h1>
-			<h2 className='text-center fw-bold pt-lg-2   location_heading mb-lg-3'> Domestic Flights</h2>
-			<div id="domesticGridCarousol" className="carousel  carousel-dark slide">
+			<div id="featureUniversities" className="carousel  carousel-dark slide">
+				<div className="carousel-indicators carButton">
+					<button type="button" data-bs-target="#featureUniversities" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+					<button type="button" data-bs-target="#featureUniversities" data-bs-slide-to="1" aria-label="Slide 2"></button>
+					<button type="button" data-bs-target="#featureUniversities" data-bs-slide-to="2" aria-label="Slide 3"></button>
+					<button type="button" data-bs-target="#featureUniversities" data-bs-slide-to="3" aria-label="Slide 4"></button>
+				</div>
 				<div className="carousel-inner">
-					<div className="carousel-item active" data-bs-interval="1000">
+					<div className="carousel-item active" data-bs-interval="2000">
 						<GridCarousolItem
-							imgOne={Barisal}
-							locationOne="Barishal"
-							fareOne="11995"
-
-							imgTwo={Chattogram}
-							locationTwo="Chattogram"
-							fareTwo="9995"
-
-							imgThree={Jessore}
-							locationThree="Jessore"
-							fareThree="10995"
-
-
+							imgOne={CollegeOne}
+							imgTwo={CollegeTwo}
+							imgThree={CollegeThree}
 						></GridCarousolItem>
 					</div>
-					<div className="carousel-item" data-bs-interval="1000">
+					<div className="carousel-item" data-bs-interval="2000">
 						<GridCarousolItem
-							imgOne={Saidpur}
-							locationOne="Saidpur"
-							fareOne="11995"
-
-							imgTwo={Sylhet}
-							locationTwo="Sylhet"
-							fareTwo="9995"
-
-							imgThree={Thakurgaon}
-							locationThree="Thakurgaon"
-							fareThree="10995"
-
-							imgFour={Jessore}
-							locationFour="Jessore"
-							fareFour="10995"
+							imgOne={CollegeFour}
+							imgTwo={CollegeFive}
+							imgThree={CollegeSix}
 						></GridCarousolItem>
 					</div>
-					<div className="carousel-item" data-bs-interval="1000">
+					<div className="carousel-item" data-bs-interval="2000">
 						<GridCarousolItem
-							imgOne={Saidpur}
-							locationOne="Saidpur"
-							fareOne="11995"
-
-							imgTwo={Chattogram}
-							locationTwo="Chattogram"
-							fareTwo="9995"
-
-							imgThree={Rajshahi}
-							locationThree="Rajshahi"
-							fareThree="10995"
-
-							imgFour={Jessore}
-							locationFour="Jessore"
-							fareFour="10995"
+							imgOne={CollegeSeven}
+							imgTwo={CollegeEight}
+							imgThree={CollegeNine}
+						></GridCarousolItem>
+					</div>
+					<div className="carousel-item" data-bs-interval="2000">
+						<GridCarousolItem
+							imgOne={CollegeTwo}
+							imgTwo={CollegeFive}
+							imgThree={CollegeSix}
 						></GridCarousolItem>
 					</div>
 
 				</div>
-				<button className="carousel-control-prev" type="button" data-bs-target="#domesticGridCarousol" data-bs-slide="prev">
+				{/* <button className="carousel-control-prev" type="button" data-bs-target="#featureUniversities" data-bs-slide="prev">
 					<span className="carousel-control-prev-icon" aria-hidden="true"></span>
 					<span className="visually-hidden">Previous</span>
 				</button>
-				<button className="carousel-control-next" type="button" data-bs-target="#domesticGridCarousol" data-bs-slide="next">
+				<button className="carousel-control-next" type="button" data-bs-target="#featureUniversities" data-bs-slide="next">
 					<span className="carousel-control-next-icon" aria-hidden="true"></span>
 					<span className="visually-hidden">Next</span>
-				</button>
+				</button> */}
 			</div>
 
-
-			{/* ########################## internationl locations flights ################################ */}
-			<h2 className='text-center fw-bold pt-lg-5 nag_heading  location_heading mb-lg-3'> International Flights</h2>
-			<div id="internationalGridCarousol" className="carousel  carousel-dark slide">
-				<div className="carousel-inner">
-					<div className="carousel-item active" data-bs-interval="1000">
-						<GridCarousolItem
-							imgOne={Singapore}
-							locationOne="Singapore"
-							fareOne="101995"
-
-							imgTwo={Beijing}
-							locationTwo="Beijing"
-							fareTwo="99995"
-
-							imgThree={Delhi}
-							locationThree="Delhi"
-							fareThree="110995"
-
-							imgFour={HongKong}
-							locationFour="HongKong"
-							fareFour="100995"
-
-						></GridCarousolItem>
-					</div>
-					<div className="carousel-item" data-bs-interval="1000">
-						<GridCarousolItem
-							imgOne={Mumbai}
-							locationOne="Mumbai"
-							fareOne="20995"
-
-							imgTwo={Singapore}
-							locationTwo="Singapore"
-							fareTwo="45995"
-
-							imgThree={Jeddah}
-							locationThree="Jeddah"
-							fareThree="70995"
-
-							imgFour={Shanghai}
-							locationFour="Shanghai"
-							fareFour="80995"
-						></GridCarousolItem>
-					</div>
-					<div className="carousel-item" data-bs-interval="1000">
-						<GridCarousolItem
-							imgOne={Dubai}
-							locationOne="Dubai"
-							fareOne="60995"
-
-							imgTwo={Doha}
-							locationTwo="Doha"
-							fareTwo="50095"
-
-							imgThree={Bangalore}
-							locationThree="Bangalore"
-							fareThree="18995"
-
-							imgFour={Jeddah}
-							locationFour="Jeddah"
-							fareFour="75995"
-						></GridCarousolItem>
-					</div>
-
-				</div>
-				<button className="carousel-control-prev" type="button" data-bs-target="#internationalGridCarousol" data-bs-slide="prev">
-					<span className="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span className="visually-hidden">Previous</span>
-				</button>
-				<button className="carousel-control-next" type="button" data-bs-target="#internationalGridCarousol" data-bs-slide="next">
-					<span className="carousel-control-next-icon" aria-hidden="true"></span>
-					<span className="visually-hidden">Next</span>
-				</button>
-			</div>
 		</div>
 
 
--- src/views/pages/Home/HomePageFeaturedUniversities/HomePageFeaturedUniversities.js
+++ src/views/pages/Home/HomePageFeaturedUniversities/HomePageFeaturedUniversities.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './HomePageFeaturedUniversities.css';
+import GridCarousol from '../../../components/GridCarousol/GridCarousol';
 
 const HomePageFeaturedUniversities = () => {
     return (
         <div>
-            <h2 className=' text-center featured_Head'>Featured Universities</h2>
+            <h2 className=' text-center featured_Head mb-md-5'>Featured Universities</h2>
+            <GridCarousol></GridCarousol>
         </div>
     );
 };
