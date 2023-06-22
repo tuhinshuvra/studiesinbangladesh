20dd92947eb81368e17880d0282880025302c1c0
tuhinshuvra <tuhinshuvra18@gmail.com>
2023-06-22 17:09:47 +0600

    some update

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Navbar from '../views/components/Shared/Navbar/Navbar';
 import Footer from '../views/components/Shared/Footer/Footer';
 import { Outlet } from 'react-router-dom';
+import './MainLayout.css';
 
 const MainLayout = () => {
     return (
         <div className='container'>
             <Navbar></Navbar>
-            <Outlet></Outlet>
+            <div className='commonMarginTop'>
+                <Outlet></Outlet>
+            </div>
             <Footer></Footer>
         </div>
     );
