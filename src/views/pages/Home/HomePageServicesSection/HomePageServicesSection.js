04444a2b97fc4dadaaa2cc49282dead23f6f8651
tuhinshuvra <tuhinshuvra18@gmail.com>
2023-06-22 17:21:53 +0600

    added some update

--- src/views/pages/Home/HomePageServicesSection/HomePageServicesSection.js
+++ src/views/pages/Home/HomePageServicesSection/HomePageServicesSection.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FaArrowCircleRight } from 'react-icons/fa';
-import './HomePageServicesSection.css';
 import HomePageServiceSectionModalsData from './HomePageServiceSectionModalsData';
+import './HomePageServicesSection.css';
 
 const HomePageServicesSection = () => {
     return (
